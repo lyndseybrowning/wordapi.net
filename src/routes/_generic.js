363e7278493bc57cbe52a27b345d628ba933746d
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-12 14:24:41 +0000

    Store word validity in variable for readability

--- src/routes/_generic.js
+++ src/routes/_generic.js
@@ -3,10 +3,11 @@ import trie from '../trie';
 const generics = (app) => {
   app.get('/api/valid/:word', (req, res) => {
     const word = req.params.word;
+    const valid = trie.contains(word);
 
     res.send({
       word,
-      valid: trie.contains(word)
+      valid
     });
   });
 }
