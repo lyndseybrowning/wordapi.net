b99ae7552db1a8b48f1d87f78b90499966825b63
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-14 10:20:11 +0000

    Import custom validator into express-validator

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import expressValidator from 'express-validator';
 import config from './config';
 import routes from './routes/main';
 import errorHandler from './middleware/errorHandler';
+import customValidators from './middleware/customValidators';
 
 const app = express();
 const port = process.env.PORT || config.port;
@@ -13,7 +14,7 @@ const server = {
     app.server = http.createServer(app);
     app.use('/', express.static(`${__dirname}/public`));
     app.use(errorHandler);
-    app.use(expressValidator());
+    app.use(expressValidator({ customValidators }));
     app.set('json spaces', 2);
     app.server.listen(port);
     routes.init(app);
