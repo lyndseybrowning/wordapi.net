65b3147b89ac1a1863b737bb1800b0cc8eaab219
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-14 10:25:17 +0000

    Validate lists parameters

--- src/routes/_lists.js
+++ src/routes/_lists.js
@@ -9,11 +9,26 @@ function filterByLength(length, word) {
 
 const lists = (app) => {
   app.get('/api/lists', (req, res) => {
-    const length = parseInt(req.query.length, 10);
-    const wordList = _dictionary.filter(filterByLength.bind(null, length));
+    const length = req.query.length;
+    const prefix = req.query.prefix;
+    const suffix = req.query.suffix;
+
+    req.check('required', 'At least one parameter is required: length, prefix, suffix').atLeastOneRequired(length, prefix, suffix);
+    req.checkQuery('prefix', 'Prefix should be a string').optional().isAlpha();
+    req.checkQuery('suffix', 'Suffix should be a string').optional().isAlpha();
+    req.checkQuery('length', 'Length should be a Number').optional().isInt();
+
+    const errors = req.validationErrors();
+
+    if(errors) {
+      return res.send({
+        errors: errors
+      });
+    }
+
+    const wordList = _dictionary.filter(filterByLength.bind(null, parseInt(length, 10)));
 
     res.send({
-      wordLength: length,
       wordsFound: wordList.length,
       wordList
     });
