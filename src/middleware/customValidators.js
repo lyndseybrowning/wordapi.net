bc2721a3c988b2f41161c072ae03b03d14af1618
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-14 10:19:43 +0000

    Added custom validator to check at least one field is available in values passed in

---
+++ src/middleware/customValidators.js
@@ -0,0 +1,9 @@
+const customValidators = {
+  atLeastOneRequired(...params) {
+    const filter = params.filter(param => (typeof param !== 'undefined'));
+
+    return filter.length > 0;
+  }
+}
+
+export default customValidators;
