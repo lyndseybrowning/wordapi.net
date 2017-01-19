2fbb2a596f4b7749a12820e88039b6bfd6433e2a
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-19 19:47:29 +0000

    Modified esLint config

--- eslint/rules.js
+++ eslint/rules.js
@@ -1,17 +1,21 @@
 module.exports = {
   "rules": {
-    "quotes": [2, "single"],
-    "no-var": 2,
-    "prefer-const": 2,
-    "no-empty-function": 2,
-    // prefer === vs ==
-    "eqeqeq": 2,
-    // no const reassignment
-    "no-const-assign": 2,
+    // always return from array method callbacks
+    "array-callback-return": 2,
     // use variables within the scope they are defined
     "block-scoped-var": 2,
+    // prefer === vs ==
+    "eqeqeq": 2,
+    // use named function expressions instead of function declarations
+    "func-style": 2,
+    // prefer [] vs new Array()
+    "no-array-constructor": 2,
+    // no const reassignment
+    "no-const-assign": 2,
     // disallow else after a return statement in if block
     "no-else-return": 2,
+    // as it says
+    "no-empty-function": 2,
     // no unnecessary bind() methods
     "no-extra-bind": 2,
     // no switch fall-through
@@ -20,17 +24,23 @@ module.exports = {
     "no-loop-func": 2,
     // prefer {} vs new Object();
     "no-new-object": 2,
+    // never use function constructor e.g. new Function()
+    "no-new-func": 2,
+    // never reassign parameters
+    "no-param-reassign": 2,
+    "no-var": 2,
     // prefer object() shorthand function declarations
     "object-shorthand": 2,
-    // quotes around property names only when needed & for reserved words
-    "quote-props": [2, "as-needed", { "keywords": true }],
-    // prefer [] vs new Array()
-    "no-array-constructor": 2,
-    // always return from array method callbacks
-    "array-callback-return": 2,
+
+    "prefer-const": 2,
+    // prefer rest params vs arguments object
+    "prefer-rest-params": 2,
     // prefer template strings vs concatenation
     "prefer-template": 2,
+    "quotes": [2, "single"],
+    // quotes around property names only when needed & for reserved words
+    "quote-props": [2, "as-needed", { "keywords": true }],
     // prefer ${name} vs ${ name }
-    "template-curly-spacing": 2
+    "template-curly-spacing": 2,
   }
 };
