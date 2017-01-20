a6971a41954875d5a67ebeb12624a80a3c727296
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-20 17:26:21 +0000

    ESLint rules

--- eslint/rules.js
+++ eslint/rules.js
@@ -1,11 +1,12 @@
 module.exports = {
   "rules": {
     // always return from array method callbacks
-    "array-callback-return": 2,  
+    "array-callback-return": 2,
     // require () around arrow function with more than one argument
     "arrow-parens": [2, "as-needed"],
     // use variables within the scope they are defined
     "block-scoped-var": 2,
+    "dot-notation": 2,
     // prefer === vs ==
     "eqeqeq": 2,
     // use named function expressions instead of function declarations
@@ -14,6 +15,8 @@ module.exports = {
     "no-array-constructor": 2,
     // no const reassignment
     "no-const-assign": 2,
+    "no-dupe-class-members": 2,
+    "no-duplicate-imports": 2,
     // disallow else after a return statement in if block
     "no-else-return": 2,
     // as it says
@@ -30,9 +33,18 @@ module.exports = {
     "no-new-func": 2,
     // never reassign parameters
     "no-param-reassign": 2,
+    // ++ or --. +=1 is more expressive. Allow ++/-- in a for loop
+    "no-plusplus": [2, { "allowForLoopAfterthoughts": true }],
+    // no undefined variables
+    "no-undef": 2,
+    // no empty class constructor
+    "no-useless-constructor": 2,
     "no-var": 2,
     // prefer object() shorthand function declarations
     "object-shorthand": 2,
+    // one declaration for each variable, don't combine
+    "one-var": [2, "never"],
+    // prefer arrow function instead of function expression
     "prefer-arrow-callback": 2,
     "prefer-const": 2,
     // prefer rest params vs arguments object
