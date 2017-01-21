421bf86e9c4ff6c200a63841de1580c8ee6a3c35
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-21 09:52:06 +0000

    Break up query into multiple lines

--- src/routes/_list.js
+++ src/routes/_list.js
@@ -7,7 +7,10 @@ const list = app => {
     const prefix = req.query.prefix;
     const suffix = req.query.suffix;
 
-    req.check('length, prefix, suffix', 'At least one parameter is required').atLeastOneRequired(length, prefix, suffix);
+    req
+      .check('length, prefix, suffix', 'At least one parameter is required')
+      .atLeastOneRequired(length, prefix, suffix);
+
     req.checkQuery('prefix', 'Prefix should be a string').optional().isAlpha();
     req.checkQuery('suffix', 'Suffix should be a string').optional().isAlpha();
     req.checkQuery('length', 'Length should be a Number').optional().isInt();
