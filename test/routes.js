84cf9a67ad2d14fa8d762e71b38f7cfc5951c532
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-22 10:33:55 +0000

    First test

--- test/routes.js
+++ test/routes.js
@@ -1,4 +1,13 @@
-import supertest from 'supertest';
-import config from '../src/config.js';
+import request from 'supertest';
+import server from '../src/server.js';
 
-const app = supertest(`http://localhost:${config.port}`);
+const app = server.init();
+
+describe('GET /api/valid/:word', () => {
+  it('responds with json', (done) => {
+    request(app)
+      .get('/api/valid/word')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
