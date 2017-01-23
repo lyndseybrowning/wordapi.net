53887a12183ba75ff398c221399c913be32b0144
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-23 18:28:32 +0000

    Test valid route

--- test/routes.js
+++ test/routes.js
@@ -1,13 +1,23 @@
 import request from 'supertest';
-import server from '../src/server.js';
-
-const app = server.init();
+import { expect } from 'chai';
+import app from '../src/index';
 
 describe('GET /api/valid/:word', () => {
   it('responds with json', (done) => {
     request(app)
-      .get('/api/valid/word')
+      .get('/api/valid/car')
       .expect('Content-Type', /json/)
-      .expect(200, done);
-  });
+      .expect(200)
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        expect(res.body).to.deep.equal({
+          word: 'car',
+          valid: true
+        });
+        done();
+      });
+    }
+  );
 });
