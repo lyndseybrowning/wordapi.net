66193f7238c44ca0f18ba1c61371d7bc2edfdaa0
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-23 19:40:39 +0000

    Force word to lowercase

--- src/routes/_generic.js
+++ src/routes/_generic.js
@@ -2,7 +2,7 @@ import trie from '../trie';
 
 const generics = (app) => {
   app.get('/api/valid/:word', (req, res) => {
-    const word = req.params.word;
+    const word = req.params.word.toLowerCase();
     const valid = trie.contains(word);
 
     res.send({
