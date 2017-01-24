9c8684359058b70af709d5907eb245b2ce2cef2d
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-24 18:56:35 +0000

    Restructure tests

--- test/api.js
+++ test/api.js
@@ -1,1 +1,11 @@
-import './api/valid';
+import fs from 'fs';
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../src/index';
+
+const api = `${__dirname}/api/`;
+const tests = fs.readdirSync(api);
+
+tests.forEach((test) => {
+  require(`./api/${test}`)(request, expect, app);
+});
