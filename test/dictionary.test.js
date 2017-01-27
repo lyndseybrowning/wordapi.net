9463d116b473907059d9b89555891faf948d02dc
Lyndsey Browning <lbrowning86@gmail.com>
2017-01-27 18:16:25 +0000

    Tests

--- test/dictionary.test.js
+++ test/dictionary.test.js
@@ -4,34 +4,61 @@ import dictionary from '../src/dictionary';
 describe('Dictionary', () => {
   let words;
 
-  beforeEach((done) => {
+  beforeEach(() => {
     words = dictionary.init();
-    done();
   });
 
-	describe('init()', () => {
-  	it('should return an array');
-    it('should not return an empty array');
+	describe('#init()', () => {
+    it('should return an array', () => {
+      expect(words).to.be.a('array');
+    });
+
+    it('should not return an empty array', () => {
+      expect(words.length).not.equal(0);
+    });
   });
 
-  describe('get()', () => {
-    it('should create an instance of the dictionary if it is empty');
-    it('should return an instance of the dictionary created by init()');
+  describe('#get()', () => {
+    it('should return an array', () => {
+      expect(dictionary.get()).to.be.a('array');
+    });
+
+    it('should return an instance of the dictionary created by init()', () => {
+      expect(dictionary.get().length).to.equal(words.length);
+    });
   });
 
-  describe('filterByLength()', () => {
-    it('should not modify the global dictionary array');
-    it('should accept a parameter of type Number');
-    it('should return an array of words filtered by length');
+  describe('#filterByLength()', () => {
+    it('should return an array', () => {
+      const actual = dictionary.filterByLength(10);
+
+      expect(actual).to.be.a('array');
+    });
+
+    it('should return an array of words filtered by length', () => {
+      const words = ['BOAT', 'CAR', 'BUS'];
+      const length = 3;
+      const expected = ['CAR', 'BUS'];
+      const actual = dictionary.filterByLength(length, words);
+
+      expect(expected).to.deep.equal(actual);
+    });
+
+    it('should not modify the global dictionary array', () => {
+      const filtered = dictionary.filterByLength(10);
+      const get = dictionary.get();
+
+      expect(get.length).to.equal(words.length);
+    });
   });
 
-  describe('filterByPrefix()', () => {
+  describe('#filterByPrefix()', () => {
     it('should not modify the global dictionary array');
     it('should accept a parameter of type String');
     it('should return an array of words that start with the requested prefix');
   });
 
-  describe('filterBySuffix()', () => {
+  describe('#filterBySuffix()', () => {
     it('should not modify the global dictionary array');
     it('should accept a parameter of type String');
     it('should return an array of words that end with the requested suffix');
