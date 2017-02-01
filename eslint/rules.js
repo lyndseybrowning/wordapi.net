4c596dd46c6d5e96ff9f97cc9c2cf94b98d4b5f0
Lyndsey Browning <lbrowning86@gmail.com>
2017-02-01 18:34:19 +0000

    Add catch/throw to keyword-spacing rules

--- eslint/rules.js
+++ eslint/rules.js
@@ -26,7 +26,7 @@ module.exports = {
     // soft tab - 2 spaces
     "indent": [2, 2],
     // enforce spaces before and after but not for if, while and for
-    "keyword-spacing": [2, { "overrides": { "if": { "after": false }, "for": { "after": false }, "while": { "after": false } } }],
+    "keyword-spacing": [2, { "overrides": { "if": { "after": false }, "catch": { "after": false }, "throw": { "after": false }, "for": { "after": false }, "while": { "after": false } } }],
     // max length of code
     "max-len": [2, 80],
     // use PascalCase for constructors or classes
