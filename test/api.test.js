400f1e7ee9e6a5638513349535c61a3189021aea
Lyndsey Browning <lbrowning86@gmail.com>
2017-02-08 19:25:32 +0000

    Pass app as method into tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import request from 'supertest';
 import { expect } from 'chai';
-import app from '../src/index';
+import app from '../src/server';
 
 const path = `${__dirname}/api`;
 const tests = fs.readdirSync(path);
 
 tests.forEach((test) => {
-  require(`./api/${test}`)(request, expect, app);
+  require(`./api/${test}`)(request, expect, app());
 });
