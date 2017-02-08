3de23e2b76afcc80d74aec29fc05e73d28b4064b
Lyndsey Browning <lbrowning86@gmail.com>
2017-02-08 19:51:35 +0000

    Test live vs dev handler

--- test/units/errorHandler.test.js
+++ test/units/errorHandler.test.js
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
 import errorHandler from '../../src/middleware/errorHandler';
+import config from '../../src/config';
 
-describe('Error handler', () => {
+describe('Error handler development', () => {
   const err = {
     message: 'This is an error',
     stack: 'stack details',
@@ -16,9 +17,33 @@ describe('Error handler', () => {
 
     send(handler) {
       expect(handler).to.be.a('object');
+      expect(handler.error).to.equal('stack details');
       return this;
     }
   };
 
   const actual = errorHandler(err, {}, res, null);
+});
+
+describe('Error handler live', () => {
+  const err = {
+    message: 'This is an error',
+    stack: 'stack details should not be shown',
+    status: 404
+  };
+
+  const res = {
+    status(result) {
+      expect(result).to.equal(err.status);
+      return this;
+    },
+
+    send(handler) {
+      expect(handler).to.be.a('object');
+      expect(handler.error).to.be.empty;
+      return this;
+    }
+  };
+
+  errorHandler(err, {}, res, null, 'production');
 });
