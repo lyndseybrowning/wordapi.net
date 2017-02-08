84a9dfee038f31788859b5e788b0555f4e32a634
Lyndsey Browning <lbrowning86@gmail.com>
2017-02-08 19:25:18 +0000

    Moved server init out of server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import server from './server';
+import config from './config';
 
 const app = server();
+const port = process.env.PORT || config.port;
+
+app.server.listen(port);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 import http from 'http';
 import express from 'express';
 import expressValidator from 'express-validator';
-import config from './config';
 import routes from './routes/main';
 import errorHandler from './middleware/errorHandler';
 import customValidators from './middleware/customValidators';
 
 const app = express();
-const port = process.env.PORT || config.port;
 
 export default () => {
   app.server = http.createServer(app);
@@ -15,9 +13,7 @@ export default () => {
   app.use(errorHandler);
   app.use(expressValidator({ customValidators }));
   app.set('json spaces', 2);
-  if(config.env !== 'test' && !module.parent) {
-    app.server.listen(port);
-  }
+
   routes.init(app);
 
   return app;
