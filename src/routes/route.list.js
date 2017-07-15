b0345dbc4945f9b36f48691d13d19103ae9d8291
Lyndsey Browning <lbrowning86@gmail.com>
2017-07-15 15:57:48 +0100

    Refactor route.list

--- src/routes/route.list.js
+++ src/routes/route.list.js
@@ -8,39 +8,45 @@ module.exports = (app, dictionary) => {
     const suffix = req.query.suffix;
 
     req
-      .check('length, prefix, suffix', 'At least one parameter is required')
+      .check('', 'Expected one or more parameters: length, prefix and suffix')
       .atLeastOneRequired(length, prefix, suffix);
 
-    req.checkQuery('prefix', 'Prefix should be a string').optional().isAlpha();
-    req.checkQuery('suffix', 'Suffix should be a string').optional().isAlpha();
-    req.checkQuery('length', 'Length should be a Number').optional().isInt();
+    req.checkQuery('prefix', 'prefix should be a string').optional().isAlpha();
+    req.checkQuery('suffix', 'suffix should be a string').optional().isAlpha();
+    req.checkQuery('length', 'length should be a Number').optional().isInt();
 
-    const errors = req.validationErrors();
+    req.getValidationResult().then((result) => {
+      if (!result.isEmpty()) {
+        const errors = result.array()[0];
 
-    if (errors) {
-      return res.status(500).send({
-        url: req.url,
-        errors,
+        return res.status(400).send({
+          url: req.url,
+          errors: `${errors.msg}`,
+        });
+      }
+
+      let wordList = [];
+
+      if (prefix) {
+        wordList = dictionary.getPrefix(prefix, false);
+      }
+
+      if (wordList.length === 0) {
+          wordList = dictionary.getWords(false);
+      }
+
+      if (length) {
+        wordList = utils.filterByLength(wordList, parseInt(length, 10));
+      }
+
+      if (suffix) {
+        wordList = utils.filterBySuffix(wordList, suffix);
+      }
+
+      res.send({
+        wordsFound: wordList.length,
+        wordList,
       });
-    }
-
-    let wordList = [];
-
-    if (prefix) {
-      wordList = dictionary.getPrefix(prefix, false);
-    }
-
-    if (length) {
-      wordList = utils.filterByLength(wordList, parseInt(length, 10));
-    }
-
-    if (suffix) {
-      wordList = utils.filterBySuffix(wordList, suffix);
-    }
-
-    res.send({
-      wordsFound: wordList.length,
-      wordList,
     });
   });
 };
