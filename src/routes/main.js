5f4820758e4506bc393a3461b17aa86b4c4d0a7e
Lyndsey Browning <lbrowning86@gmail.com>
2017-08-12 16:45:48 +0100

    Update deploy

--- src/routes/main.js
+++ src/routes/main.js
@@ -20,13 +20,15 @@ const routes = {
   init(app) {
     const routeFiles = fs.readdirSync(__dirname);
 
-    app.get('/', (req, res) => {
+    app.get('/', (req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); 
       res.setHeader(
         'Access-Control-Allow-Headers', 
         'X-Requested-With,contenttype'
       );
+
+      next();
     });
     
     routeFiles
